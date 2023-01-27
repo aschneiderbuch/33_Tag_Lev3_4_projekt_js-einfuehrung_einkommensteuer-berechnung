9995de75bd5d24ab55a5e1793984eefb62325e13
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-27 11:34:55 +0100

    htmlGerüstEnde

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,52 @@
 console.log("test");
+
+/* 
+Frage
+
+Jahr anhand der Input:radio Buttons auswählen
+    1.  2020
+    2.  2021
+    3.  2022
+            Umstellen       span im Output auf das jeweilige Jahr
+
+            ja nach Jahr    !!!   Vorsicht   gibt   bei den 5 Fällen unterschiedlich! 
+                                    Einkommensgrenzen     und   Unterschiedliche Werte in den Formeln Fällen 1-5
+
+dann 
+Familienstand anhand der Input:radio Buttons auswählen
+    1. singel
+    2. Verheiratet
+
+        wenn Verheiratet   !!! Vorsicht    zvE / 2       und am     Ende    EST * 2
+
+
+
+Funktion 
+    verschieden Szenarien    sind die Fälle 1 - 5
+
+
+Output   
+        wenn Verheiratet   !!! Vorsicht    zvE / 2       und am     Ende    EST * 2
+
+
+*/
+
+
+
+// Variablen Input
+
+let radio_jahr2022 = document.querySelector("#radio_jahr2022").ariaChecked;
+console.log(radio_jahr2022);
+
+
+
+
+
+
+function test(){
+    console.log(radio_jahr2022);
+
+}
+
+
+test();
